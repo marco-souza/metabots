c8c36afe36005be8703cc6aa12934500e746f6d3
Marco Antônio <ma.souza.junior@gmail.com>
2025-07-07 19:02:29 -0300

    fix: secret sharing using direnv

--- config/config.exs
+++ config/config.exs
@@ -9,9 +9,9 @@
 # move said applications out of the umbrella.
 import Config
 
-if File.exists?("./secrets.exs") do
-  import_config("./secrets.exs")
-end
+config :binance,
+  api_key: System.get_env("BINANCE_API_KEY", ""),
+  secret_key: System.get_env("BINANCE_SECRET_KEY", "")
 
 config :logger,
   level: :debug,
